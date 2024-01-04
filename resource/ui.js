7aca10329acd38925a17fd35ddeacabcfb86dc36
hwang seungwon <hsw_4935@naver.com>
2024-01-04 09:49:46 +0900

    text update

--- resource/ui.js
+++ resource/ui.js
@@ -1,8 +1,15 @@
 function mainFn ( a,b ) {
     let targeth3 = a.querySelector('#content2024 > h3');
-    targeth3.addEventListener('click',function(){
-        navigator.clipboard.writeText('https://swon1.github.io/roadmap/');
-        alert('복사 완료');
+    
+    let clip = new ClipboardJS('#content2024 > h3');
+
+    // 성공시
+    clip.on('success', function(e) {
+        console.log("Success");
+    });
+    // 실패시
+    clip.on('error', function(e) {
+        console.log("Error");
     });
 
     console.log(targeth3);
