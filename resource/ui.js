fffc5eda60391fc9d338d78e9b0840b95010caea
hwang seungwon <hsw_4935@naver.com>
2024-01-04 13:03:20 +0900

    test update

--- resource/ui.js
+++ resource/ui.js
@@ -1,18 +1,17 @@
 function mainFn ( a,b ) {
-    let targeth3 = a.querySelector('#content2024 > h3');
-    
-    let clip = new ClipboardJS('#content2024 > h3');
+    let clip = new ClipboardJS('.textcopy');
 
     // 성공시
     clip.on('success', function(e) {
         console.log("Success");
+        console.log(e);
     });
     // 실패시
     clip.on('error', function(e) {
         console.log("Error");
     });
 
-    console.log(targeth3);
-
-    console.log(a,b);
+    a.querySelector('#content2024 > h3').addEventListener('click', () => {
+        a.querySelector('.textcopy').click();
+    });
 };
